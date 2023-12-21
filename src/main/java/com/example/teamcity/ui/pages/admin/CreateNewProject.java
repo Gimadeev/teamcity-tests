450a3ce6a2a1f9c19d400928e6f49abf97d3134f
gimadeev <6962009@gmail.com>
2023-12-22 00:40:10 +0400

    Added: CreateNewProjectTest, CreateNewBuildTest

--- src/main/java/com/example/teamcity/ui/pages/admin/CreateNewProject.java
+++ src/main/java/com/example/teamcity/ui/pages/admin/CreateNewProject.java
@@ -2,15 +2,24 @@ package com.example.teamcity.ui.pages.admin;
 
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
+import com.codeborne.selenide.selector.ByAttribute;
+import com.example.teamcity.api.models.NewProjectDescription;
 import com.example.teamcity.ui.Selectors;
 import com.example.teamcity.ui.pages.Page;
 
 import static com.codeborne.selenide.Selenide.element;
 
 public class CreateNewProject extends Page {
-    private SelenideElement urlInput = element(Selectors.byId("url"));
-    private SelenideElement projectNameInput = element(Selectors.byId("projectName"));
-    private SelenideElement buildTypeNameInput = element(Selectors.byId("buildTypeName"));
+    private final SelenideElement urlInput = element(Selectors.byId("url"));
+    private final SelenideElement projectNameInput = element(Selectors.byId("projectName"));
+    private final SelenideElement buildTypeNameInput = element(Selectors.byId("buildTypeName"));
+    private final SelenideElement createManuallyButton = element(new ByAttribute("data-hint-container-id", "create-project"));
+    private final SelenideElement nameInput = element(Selectors.byId("name"));
+    private final SelenideElement projectIdInput = element(Selectors.byId("externalId"));
+    private final SelenideElement descriptionInput = element(Selectors.byId("description"));
+    private static final SelenideElement errorName = element(Selectors.byId("errorName"));
+    private static final SelenideElement errorExternalId = element(Selectors.byId("errorExternalId"));
+
     public CreateNewProject open(String parentProjectId) {
         Selenide.open("/admin/createObjectMenu.html?projectId=" + parentProjectId + "&showMode=createProjectMenu&cameFromUrl=http%3A%2F%2Flocalhost%3A8111%2Ffavorite%2Fprojects#createManually");
         waitUntilPageIsLoaded();
@@ -30,5 +39,24 @@ public class CreateNewProject extends Page {
         buildTypeNameInput.clear();
         buildTypeNameInput.sendKeys(buildTypeName);
         submit();
+    }
+
+    public EditProject createProjectManually(NewProjectDescription newProjectDescription) {
+        waitUntilElementIsAppeared(createManuallyButton);
+        createManuallyButton.click();
+        nameInput.sendKeys(newProjectDescription.getName());
+        projectIdInput.clear();
+        projectIdInput.sendKeys(newProjectDescription.getId());
+        descriptionInput.sendKeys(newProjectDescription.getDescription());
+        submit();
+        return new EditProject();
+    }
+
+    public static String getErrorNameMessage() {
+        return errorName.getText();
+    }
+
+    public static String getErrorExternalIdMessage() {
+        return errorExternalId.getText();
     }
 }
